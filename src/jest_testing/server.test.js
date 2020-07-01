476b88ea99790b5d8e91fafe342918856e0583f0
Nisarg Patel <npatel42@vols.utk.edu>
2020-07-01 18:10:34 -0500

    Update server.test.js

--- src/jest_testing/server.test.js
+++ src/jest_testing/server.test.js
@@ -2,10 +2,10 @@ const app = require('../server/server');
 const supertest = require('supertest');
 const request = supertest(app);
 
-describe('get endpoints correct', () => {
-    it ('return html file', async done => {
-        const res =  await request.get('/')
-        expect(res.status).toBe(200);
+describe('test server', () => {
+    it ('return body message from test url', async done => {
+        const res =  await request.get('/test')
+        expect(res.body.message).toBe('pass!');
         done();
     })
 })

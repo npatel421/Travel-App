6406a6c6f10ffeeb5841825adb9592b3c091b88e
Nisarg Patel <npatel42@vols.utk.edu>
2020-07-02 13:14:12 -0500

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -3,4 +3,9 @@ import { deleteTrip } from './js/app.js'
 
 import './styles/style.scss'
 
+const buttonID = document.getElementById('generate')
+
+buttonID.addEventListener('click', formHandler)
+buttonID.addEventListener('submit', formHandler)
+
 export { formHandler, deleteTrip }
